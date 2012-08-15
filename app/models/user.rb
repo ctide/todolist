4980a0a85e65dc9e3015472d0698fbe309f3e759
ctide <christide@christide.com>
2012-08-15 15:26:36 -0700

    more minor tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ActiveRecord::Base
   has_many :undone_entries, :source => :entries, :class_name => "Entry", :conditions => { :entries => { :completed => false } }
   has_many :finished_entries, :source => :entries, :class_name => "Entry", :conditions => { :entries => { :completed => true } }
 
+  after_save :check_auth_token
+
+  private
+  def check_auth_token
+    if self.authentication_token.blank?
+      self.reset_authentication_token!
+    end
+  end
 end
