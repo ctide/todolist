f19899cb7bcebd403803d4a72689a296dc1bb3de
ctide <christide@christide.com>
2012-08-14 17:20:09 -0700

    install devise, install rails, setup pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = '78e4f89cf352c22f48c7d88e246f43d9f2aa8b0a8c3468de1c6a461469e7d641221bb747139f7105d19c1642ceac37cd835d20189730809a70a355357a394027'
